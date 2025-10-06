3748e60f4b41cc3d9a52b1a7f47436fe804c10d8
Ian Kurtz <ian.kurtz@harness.io>
2025-10-06 13:19:03 -0500

    Create versions.tf
    initial commit

---
+++ module-testing/versions.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 1.5.7"
+    }
+  }
+}
